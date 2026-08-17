9d3658de70c5d5bcf99a6e9519dac35d298dde7a
Samyukta A <asamyukta2006@gmail.com>
2026-08-17 12:05:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -1,49 +1,49 @@
 class Solution {
     public int findPages(int[] arr, int k) {
-        int n = arr.length;
-
-        if (k > n) return -1;
-
-        int low = 0, high = 0;
-
-        for (int pages : arr) {
-            low = Math.max(low, pages);
-            high += pages;
-        }
-
-        int ans = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canAllocate(arr, k, mid)) {
-                ans = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return ans;
+        int n=arr.length;
+        if(k>n){
+             return -1;
+         }
+         long max=0,sum=0;
+         for(int i=0;i<n;i++){
+             max=Math.max(max,arr[i]);
+             sum+=arr[i];
+         }
+         if(k==1){
+             return (int)sum;
+         }
+         long low=max,high=sum;
+         long ans=-1;
+         while(low<high){
+             long mid=(low+high)/2;
+             if(possible(mid,arr,k)){
+                 ans=mid;
+                 high=mid;
+             }
+             else{
+                 low=mid+1;
+             }
+         }
+         return (int)ans;
+        
     }
+    
+    static boolean possible(long check,int[] arr,int k){
 
-    private boolean canAllocate(int[] arr, int k, int maxPages) {
-        int students = 1;
-        int pages = 0;
-
-        for (int book : arr) {
-            if (pages + book <= maxPages) {
-                pages += book;
-            } else {
-                students++;
-                pages = book;
-
-                if (students > k) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
+         int size=1;
+         int i=0;
+         long sum=0;
+         while(i<arr.length){
+             if(sum+arr[i]<=check){
+                 sum=sum+arr[i];
+             }
+             else{
+                 size++;
+                 sum=arr[i];
+             }
+             i++;
+         }
+         if(size<=k) return true;
+         else return false;
     }
 }
